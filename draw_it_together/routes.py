552a3dbbf267e634271485d1e0410565244953e7
Nicholas Jones <dojoman19@gmail.com>
2018-11-07 12:04:00 -0500

    Added a common error and success function

--- draw_it_together/routes.py
+++ draw_it_together/routes.py
@@ -4,6 +4,17 @@ from flask import jsonify
 import json
 import db_ops
 
+def success():
+    return jsonify(
+        success=True
+    )
+
+def error(reason_str):
+    return jsonify(
+        success=False,
+        reason=reason_str
+    )
+
 @app.route('/')
 @app.route('/index')
 def index():
@@ -14,42 +25,44 @@ def index():
 def add_user():
     if request.headers['Content-Type'] == 'application/json':
         if not ('name' in request.json and 'id' in request.json):
-            return "Name and ID are required."
+            return error("Name and ID are required.")
         name = request.json['name']
         user_id = request.json['id']
         db_ops.db_add_user(user_id, name)
-        return "Successfully added user to the DB."
-    return "JSON body is required."
+        return success()
+    return error("JSON body is required.")
 
 @app.route('/clear_users', methods=['POST'])
 def clear_users():
     db_ops.db_clear_table('users')
-    return "Table users successfully cleared."
+    return success()
 
 @app.route('/clear_pictures', methods=['POST'])
 def clear_pictures():
     db_ops.db_clear_table('pictures')
     db_ops.db_reset_serial('pictures', 'id')
-    return "Table pictures successfully cleared."
+    return success()
 
 @app.route('/check_user', methods=['POST'])
 def check_user():
     if request.headers['Content-Type'] == 'application/json':
         if not 'name' in request.json:
-            return "Name is required."
+            return error("Name is required.")
         name = request.json['name']
         return jsonify(
             exists=db_ops.db_check_for_name(name)
         )
     else:
-        return "JSON body is required."
+        return error("JSON body is required.")
 
 @app.route('/add_picture', methods=['POST'])
 def add_picture():
     if request.headers['Content-Type'] == 'application/json':
         if not ('name' in request.json and 'picture' in request.json):
-            return "Name and picture are required"
+            return error("Name and picture are required.")
         name = request.json['name']
         img_base_64 = request.json['picture']
         db_ops.db_add_image(name, img_base_64)
-    return "Good"
+        return success()
+    else:
+        return error("JSON body is required.")
